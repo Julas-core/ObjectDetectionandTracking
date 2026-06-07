4462f55c8e361ca6e6d44452fd56353cb97ac30e
Julas-core <julasmame@gmail.com>
2026-06-07 22:23:36 +0300

    First Commit: implementing a simple object detection software using python

---
+++ main.py
@@ -0,0 +1,30 @@
+from ultralytics import YOLO
+import cv2
+import time
+
+model = YOLO('yolov8n.pt')
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
+for _ in range(5):
+    cap.read()
+    time.sleep(0.1)
+
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        time.sleep(0.03)
+        continue
+
+    results = model.track(frame, persist=True)
+    annotated = results[0].plot()
+
+    cv2.imshow('OBject Detection', annotated)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    cap.release()
+    cv2.destroyAllWindows()
